d5bc9aedebf10afe416789acd75310987fd8f745
Roys77111 <roys77111@gmail.com>
2023-04-25 19:29:50 -0700

    All Task

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,26 +1,20 @@
-# install nginx, listen on port 80
-# GET with curl return "Hello World"
-# redirection must a 301 Moved permanently
-
+# Puppet manifest to install nginx
 package { 'nginx':
-ensure => installed,
-name   => 'nginx',
+  ensure => installed,
+}
+
+file_line { 'aaaaa':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 file { '/var/www/html/index.html':
-content => 'Hello World',
-path    => '/var/www/html/index.html'
-}
-
-file_line { 'title':
-ensure   => present,
-path     => '/etc/nginx/sites-available/default',
-after    => 'server_name _;',
-line     => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-multiple => true
+  content => 'Hello World!',
 }
 
 service { 'nginx':
-ensure  => running,
-require => Package['nginx'],
+  ensure  => running,
+  require => Package['nginx'],
 }
