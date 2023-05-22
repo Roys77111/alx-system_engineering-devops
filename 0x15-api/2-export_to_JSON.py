270ac6d90af6232b6eea687794a150ad67108690
Roys77111 <roys77111@gmail.com>
2023-05-22 23:50:27 +0000

    All Task

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,36 +1,44 @@
 #!/usr/bin/python3
 """
-    Calls to data from the jsonplaceholder website parsed to json formats
-    from task 0, extend your Python script to export data in the JSON format.
+Check student JSON output
 """
+
+import json
+import requests
+import sys
+
+users_url = "https://jsonplaceholder.typicode.com/users?id="
+todos_url = "https://jsonplaceholder.typicode.com/todos"
+
+
+def user_info(id):
+    """ Check user info """
+    
+    with open(str(id) + '.json', 'r') as f:
+        student_json = json.load(f)
+
+    student_dicts = student_json[str(id)]
+    
+    response = requests.get(todos_url).json()
+    flag = 0
+    json_count = 0
+    not_found_count = 0
+    for i in response:
+        if i['userId'] == id:
+            usr_resp = requests.get(users_url + str(i['userId'])).json()
+            json_entry = {'username': usr_resp[0]['username'], 'completed': i['completed'], 'task': i['title']}
+            json_count += 1
+            flag = 0
+            for item in student_dicts:
+                if json_entry == item:
+                    flag = 1
+            if flag == 0:
+                not_found_count += 1
+
+    if not_found_count != 0:
+        print("Number of tasks missing: {}".format(not_found_count))
+    else:
+        print("All tasks found: OK")
+
 if __name__ == "__main__":
-    """
-        Calls to data from the jsonplaceholder website
-        parsed to json formats from task 0, extend your
-        Python script to export data in the JSON format.
-    """
-    import json
-    import requests
-    import sys
-
-    EMPLOYEE_ID = sys.argv[1]
-
-    link = "https://jsonplaceholder.typicode.com/users"
-    userResponse = requests.get(f"{link}/{EMPLOYEE_ID}")
-    EMPLOYEE_NAME = userResponse.json().get("name")
-    fileName = "{}.json".format(EMPLOYEE_ID)
-
-    todoList = requests.get(f"{link}/{EMPLOYEE_ID}/todos").json()
-
-    tasks = []
-    for task in todoList:
-        taskDict = {}
-        taskDict["task"] = task.get('title')
-        taskDict["completed"] = task.get('completed')
-        taskDict["username"] = EMPLOYEE_NAME
-        tasks.append(taskDict)
-
-    todoDict = {EMPLOYEE_ID: tasks}
-
-    with open(fileName, "w", newline="") as fd:
-        json.dump(todoDict, fd)
+    user_info(int(sys.argv[1]))
